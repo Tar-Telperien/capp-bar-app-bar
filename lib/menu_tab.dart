966f5fc1b9db6104a132bca5ce30030b6678e48a
Therese Relucio <63369100+trelucio@users.noreply.github.com>
2021-04-14 14:58:43 -0500

    Add menu_tab

---
+++ lib/menu_tab.dart
@@ -0,0 +1,13 @@
+// Capp Bar Menu
+// Will hold menu items and button to add menu item to cart/orders
+import 'package:flutter/material.dart';
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Capp Bar Menu')),
+    );
+  }
+}
