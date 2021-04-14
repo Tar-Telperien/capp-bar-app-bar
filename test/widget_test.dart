0d9aff21209af1b3fbb63adc19779a7f63130387
Therese Relucio <trelucio@udallas.edu>
2021-04-14 15:04:00 -0500

    Build updates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:capp_app/app.dart';
+import 'package:capp_app/capp_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
